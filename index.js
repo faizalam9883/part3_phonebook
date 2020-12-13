bb141f5142038c68f09121b924017c890efb573d
Faiz Alam <faizalam9883@gmail.com>
2020-12-13 18:18:31 +0530

    added frontend

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static("build")) // this helps in serving static files from the production build of the frontend
 
 let persons = [
 	{
